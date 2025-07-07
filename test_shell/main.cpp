983ff947a0f81636cd5a6856438beddfcd63beea
Limdongguen <vs008@naver.com>
2025-07-07 14:58:53 +0900

    Fix comment as English

--- test_shell/main.cpp
+++ test_shell/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <string>
-#include <cstdlib> // system 함수
+#include <cstdlib>
 
 #ifdef _DEBUG
-// Debug 모드(GTEST 실행) 
 #include <gmock/gmock.h>
 
 TEST(SampleTest, Addition) {
@@ -16,19 +15,18 @@ int main(int argc, char** argv) {
 }
 
 #else
-// Release 모드(명령어 Shell 실행)
 int main() {
     std::string command;
     while (true) {
-        std::cout << "명령어 입력 (done 입력 시 종료): ";
+        std::cout << " SHELL : ";
         std::getline(std::cin, command);
 
         if (command == "done") {
-            std::cout << "프로그램을 종료합니다." << std::endl;
+            std::cout << "PROGRAM DONE" << std::endl;
             break;
         }
         int result = system(command.c_str());
-        std::cout << "실행 결과 코드: " << result << std::endl;
+        std::cout << "SHELL COMMAND : " << result << std::endl;
     }
     return 0;
 }
