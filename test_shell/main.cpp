967c238167a02fe7cb7e4a1cfa329c0d9f9f5ca3
unknown <jwon96.park@gmail.com>
2025-07-08 08:47:16 +0900

    [fix] add public declaration & add header files

--- test_shell/main.cpp
+++ test_shell/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include "mock_ssd_adapter.h"
 
 #ifdef _DEBUG
 #include <gmock/gmock.h>
+
+using namespace testing;
 
 TEST(SampleTest, Addition) {
     EXPECT_EQ(2 + 2, 4);
