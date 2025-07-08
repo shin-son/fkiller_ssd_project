21b5147e49ec1c40ce83f4edffeec23230aa438c
Hyungju Song <hyungju.song54@gmail.com>
2025-07-08 10:24:02 +0900

    [refactor] apply consistent format across tests

--- ssd/main.cpp
+++ ssd/main.cpp
@@ -1,37 +1,4 @@
 #include "gmock/gmock.h"
-#include "ssd_write.h"
-
-TEST(SSDREADWRITE, WRITEPASS) {
-	SsdWrite ssdwrite;
-	std::string address = "0";
-	std::string value = "0xabc";
-
-	EXPECT_NO_THROW(ssdwrite.write(address, value));
-}
-
-TEST(SSDREADWRITE, WRITEFAILED_INVALID_ADDRESS) {
-	SsdWrite ssdwrite;
-	std::string address = "-1";
-	std::string value = "0xabc";
-
-	EXPECT_THROW(ssdwrite.write(address, value), std::out_of_range);
-}
-
-TEST(SSDREADWRITE, WRITEFAILED_INVALID_ARGUMENT_ADDRESS) {
-	SsdWrite ssdwrite;
-	std::string address = "- 1 2";
-	std::string value = "0xabc";
-
-	EXPECT_THROW(ssdwrite.write(address, value), std::invalid_argument);
-}
-
-TEST(SSDREADWRITE, INVALID_MEMORY_VALUE) {
-	SsdWrite ssdwrite;
-	std::string address = "34";
-	std::string value = "test";
-
-	EXPECT_THROW(ssdwrite.write(address, value), std::invalid_argument);
-}
 
 int main() {
 
