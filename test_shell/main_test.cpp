2e6ddc53b72e56b4fc65073461ba1be2cf824476
jw.song <chokosal@naver.com>
2025-07-08 10:18:31 +0900

    [feature] partialLBAWrite에 대한 기본 TC 추가(TC Fail상태)

--- test_shell/main_test.cpp
+++ test_shell/main_test.cpp
@@ -8,12 +8,35 @@ using namespace testing;
 TEST(SampleTest, Addition) {
     EXPECT_EQ(2 + 2, 4);
 }
-
 TEST(TS, ReadPass) {
     MockSSDAdapter mockSSDAdapter;
     TestShell testShell;
 
     EXPECT_CALL(mockSSDAdapter, read(10))
         .WillRepeatedly(Return("0xAAAABBBB"));
+}
+
+TEST(SampleTest, partialLBAWrite_CountWriteTimesWithFullCommnad) {
+    MockSSDAdapter mockSSDAdapter;
+    TestShell testShell(&mockSSDAdapter);
+    string input = "2_PartialLBAWrite";
+    string writeData = "0x12341234";
+
+    EXPECT_CALL(mockSSDAdapter, write(_, writeData))
+        .Times(150);
+
+    testShell.runCommand(input);
+}
+
+TEST(SampleTest, partialLBAWrite_CountWriteTimesWittShortCommand) {
+    MockSSDAdapter mockSSDAdapter;
+    TestShell testShell(&mockSSDAdapter);
+    string input = "2_";
+    string writeData = "0x12341234";
+
+    EXPECT_CALL(mockSSDAdapter, write(_, writeData))
+        .Times(150);
+
+    testShell.runCommand(input);
 }
 #endif
