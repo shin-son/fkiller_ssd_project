a3f823eea448d5b2b20d4b2da07700a0098552a6
unknown <jwon96.park@gmail.com>
2025-07-08 09:38:52 +0900

    [feat] add main_test.cpp

--- test_shell/main.cpp
+++ test_shell/main.cpp
@@ -1,24 +1,6 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include "mock_ssd_adapter.h"
 #include "test_shell.h"
 
-#ifdef _DEBUG
-#include <gmock/gmock.h>
-
-using namespace testing;
-
-TEST(SampleTest, Addition) {
-    EXPECT_EQ(2 + 2, 4);
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
-#else
+#ifndef _DEBUG
 int main() {
     TestShell testShell;
     testShell.runShell();
---
+++ test_shell/main_test.cpp
@@ -0,0 +1,24 @@
+#ifdef _DEBUG
+#include <cstdlib>
+#include "mock_ssd_adapter.h"
+#include "test_shell.h"
+
+using namespace testing;
+
+TEST(SampleTest, Addition) {
+    EXPECT_EQ(2 + 2, 4);
+}
+
+TEST(TS, ReadPass) {
+    MockSSDAdapter mockSSDAdapter;
+    TestShell testShell;
+
+    EXPECT_CALL(mockSSDAdapter, read(10))
+        .WillRepeatedly(Return("0xAAAABBBB"));
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+#endif
