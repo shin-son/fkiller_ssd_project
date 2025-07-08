e37cec76df4e4cd864b644e8cccd243010b28e72
sungmini.kim <clever1501@naver.com>
2025-07-08 14:05:06 +0900

    [refactoring] ssd read code / ssd read test code

--- ssd/ssd_read_test.cpp
+++ ssd/ssd_read_test.cpp
@@ -5,72 +5,60 @@
 using namespace testing;
 using std::string;
 
-TEST(SsdReadTest, ReadFileStream) {
-	
-	Ssd_Read ssdRead("ssd_nand.txt");
+class SsdReadTestFixture : public Test {
+protected:
+	SsdRead ssdRead{ "ssd_nand.txt", "ssd_output.txt" };
+
+	void SetUp() override {
+		ssdRead.readSsdNandFile();
+	}
+};
+
+TEST_F(SsdReadTestFixture, ReadFileStream) {
 
 	EXPECT_TRUE(ssdRead.readSsdNandFile());
 }
 
-TEST(SsdReadTest, WrongReadFileStream) {
-	
-	Ssd_Read ssdRead("hdd_read_test.cpp");
+TEST_F(SsdReadTestFixture, WrongReadFileStream) {
 
-	EXPECT_FALSE(ssdRead.readSsdNandFile());
+	SsdRead ssdReadWrong("hdd_read_test.cpp");
+
+	EXPECT_FALSE(ssdReadWrong.readSsdNandFile());
 }
 
-TEST(SsdReadTest, WriteFileStream) {
-	
-	Ssd_Read ssdRead("ssd_nand.txt", "ssd_output.txt");
+TEST_F(SsdReadTestFixture, WriteFileStream) {
 
 	EXPECT_TRUE(ssdRead.writeSsdNandDataToFile("test"));
 }
 
-TEST(SsdReadTest, AllDataRead) {
-	
-	Ssd_Read ssdRead;
+TEST_F(SsdReadTestFixture, AllDataRead) {
 
 	int expectedSize = 100;
 
-	EXPECT_TRUE(ssdRead.readSsdNandFile());
 	EXPECT_EQ(expectedSize, ssdRead.getSsdNandDataSize());
 }
 
-TEST(SsdReadTest, SpecificReadData) {
-
-	Ssd_Read ssdRead;
+TEST_F(SsdReadTestFixture, SpecificReadData) {
 
 	string expectedString = "0x00000000";
 
-	EXPECT_TRUE(ssdRead.readSsdNandFile());
 	EXPECT_EQ(expectedString, ssdRead.getSsdNandDataAt(2));
 }
 
-TEST(SsdReadTest, OutOfRangeReadData) {
+TEST_F(SsdReadTestFixture, OutOfRangeReadData) {
 
-	Ssd_Read ssdRead;
-
-	EXPECT_TRUE(ssdRead.readSsdNandFile());
 	EXPECT_THROW(ssdRead.getSsdNandDataAt(100), std::out_of_range);
 }
 
-TEST(SsdReadTest, ReadDataWriteToOutputFile) {
-	
-	Ssd_Read ssdRead;
+TEST_F(SsdReadTestFixture, ReadDataWriteToOutputFile) {
 
-	EXPECT_TRUE(ssdRead.readSsdNandFile());
-	
 	string expectedString = ssdRead.getSsdNandDataAt(2);
 
 	EXPECT_TRUE(ssdRead.isSsdOutputFileCorrect(expectedString));
 }
 
-TEST(SsdReadTest, WrongReadDataWriteToOutputFile) {
-	
-	Ssd_Read ssdRead;
+TEST_F(SsdReadTestFixture, WrongReadDataWriteToOutputFile) {
 
-	EXPECT_TRUE(ssdRead.readSsdNandFile());
-	
 	string expectedString = "ERROR";
 
 	EXPECT_THROW(ssdRead.getSsdNandDataAt(100), std::out_of_range);
